a62930d5c53198c97b0bc880a39efc0abd1f867c
JiangfengDong <jfdong829@gmail.com>
2022-04-14 20:02:00 -0700

    1. Add TinNewsApplication 2. Update the AndroidManifest to point to the extended TinNewsApplication

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/TinNewsApplication.java
@@ -0,0 +1,10 @@
+package edu.neu.madcourse.numad22sp_team36_tinnews;
+
+import android.app.Application;
+
+public class TinNewsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
