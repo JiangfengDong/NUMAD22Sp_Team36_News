463aca9d64fc07ff3fb5b1bcc8039ce2063c6f86
JiangfengDong <jfdong829@gmail.com>
2022-04-14 19:09:20 -0700

    Add NewsAPI interface

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/network/NewsAPI.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.numad22sp_team36_tinnews.network;
+
+import edu.neu.madcourse.numad22sp_team36_tinnews.model.NewsResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NewsAPI {
+
+    @GET("top-headlines")
+    Call<NewsResponse> getTopHeadlines(@Query("country") String country);
+
+    @GET("everything")
+    Call<NewsResponse> getEverything(@Query("q") String query, @Query("pageSize") int pageSize);
+
+}
