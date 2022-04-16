a4f79e35539c6e69dd2177769528e92e8393749b
JiangfengDong <jfdong829@gmail.com>
2022-04-16 00:43:29 -0700

    1. Add one more interceptor in RetrofitClient 2. Add the initialization code in the onCreate in the TinNewsApplication

--- app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/TinNewsApplication.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/TinNewsApplication.java
@@ -2,9 +2,12 @@ package edu.neu.madcourse.numad22sp_team36_tinnews;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+
 public class TinNewsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
     }
 }
