b405e2f37b36a233bc84130b4b382d6077859f63
JiangfengDong <jfdong829@gmail.com>
2022-04-15 23:47:16 -0700

    Implement a factory for creating ViewModels

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/repository/NewsViewModelFactory.java
@@ -0,0 +1,29 @@
+package edu.neu.madcourse.numad22sp_team36_tinnews.repository;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import edu.neu.madcourse.numad22sp_team36_tinnews.ui.home.HomeViewModel;
+import edu.neu.madcourse.numad22sp_team36_tinnews.ui.search.SearchViewModel;
+
+public class NewsViewModelFactory implements ViewModelProvider.Factory {
+
+    private final NewsRepository repository;
+
+    public NewsViewModelFactory(NewsRepository repository) {
+        this.repository = repository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(HomeViewModel.class)) {
+            return (T) new HomeViewModel(repository);
+        } else if (modelClass.isAssignableFrom(SearchViewModel.class)) {
+            return (T) new SearchViewModel(repository);
+        } else {
+            throw new IllegalStateException("Unknown ViewModel");
+        }
+    }
+}
