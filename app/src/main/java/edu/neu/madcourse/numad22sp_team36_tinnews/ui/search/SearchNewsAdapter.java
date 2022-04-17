8d11d7976ca573d614c3ef22063b4dfbebd3b6df
JiangfengDong <jfdong829@gmail.com>
2022-04-16 22:59:35 -0700

    Implement a RecyclerView: Link the item_view layout to the ViewHolder and finish create/bind ViewHolder process in the RecyclerView

--- app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/ui/search/SearchNewsAdapter.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/ui/search/SearchNewsAdapter.java
@@ -1,6 +1,8 @@
 package edu.neu.madcourse.numad22sp_team36_tinnews.ui.search;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,16 +12,36 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.neu.madcourse.numad22sp_team36_tinnews.R;
 import edu.neu.madcourse.numad22sp_team36_tinnews.databinding.SearchNewsItemBinding;
 import edu.neu.madcourse.numad22sp_team36_tinnews.model.Article;
 
-public class SearchNewsAdapter {
+public class SearchNewsAdapter extends RecyclerView.Adapter<SearchNewsAdapter.SearchNewsViewHolder> {
 
     private List<Article> articles = new ArrayList<>();
 
     public void setArticles(List<Article> newsList) {
         articles.clear();
         articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public SearchNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_news_item, parent, false);
+        return new SearchNewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchNewsViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.itemTitleTextView.setText(article.title);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
     }
 
     public static class SearchNewsViewHolder extends RecyclerView.ViewHolder {
