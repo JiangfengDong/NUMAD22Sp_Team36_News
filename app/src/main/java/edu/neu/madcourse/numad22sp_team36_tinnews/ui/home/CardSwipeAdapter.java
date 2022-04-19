4600a32570ff49aad4f246bfc389d04809326d99
JiangfengDong <jfdong829@gmail.com>
2022-04-18 18:48:10 -0700

    Link the item_view layout swipe_news_cards.xml to the ViewHolder and finish create/bind ViewHolder process in the RecyclerView

--- app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/ui/home/CardSwipeAdapter.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/ui/home/CardSwipeAdapter.java
@@ -1,25 +1,53 @@
 package edu.neu.madcourse.numad22sp_team36_tinnews.ui.home;
 
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.neu.madcourse.numad22sp_team36_tinnews.R;
 import edu.neu.madcourse.numad22sp_team36_tinnews.databinding.SwipeNewsCardBinding;
 import edu.neu.madcourse.numad22sp_team36_tinnews.model.Article;
 
-public class CardSwipeAdapter {
+public class CardSwipeAdapter extends RecyclerView.Adapter<CardSwipeAdapter.CardSwipeViewHolder> {
 
     private List<Article> articles = new ArrayList<>();
 
     public void setArticles(List<Article> newsList) {
         articles.clear();
         articles.addAll(newsList);
+    }
+
+    @NonNull
+    @Override
+    public CardSwipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_news_card, parent, false);
+        return new CardSwipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardSwipeViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.titleTextView.setText(article.title);
+        holder.descriptionTextView.setText(article.description);
+        if (!TextUtils.isEmpty(article.urlToImage)) {
+            Picasso.get().load(article.urlToImage).into(holder.imageView);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
     }
 
     public static class CardSwipeViewHolder extends RecyclerView.ViewHolder {
