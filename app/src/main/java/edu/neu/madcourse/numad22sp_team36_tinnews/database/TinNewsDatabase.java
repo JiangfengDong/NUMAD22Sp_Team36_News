f2fa57a1ddd0c6a5f313ed4390dc3fc8a0bd571c
JiangfengDong <jfdong829@gmail.com>
2022-04-21 20:20:24 -0700

    Add TinNewsDatabase

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/database/TinNewsDatabase.java
@@ -0,0 +1,12 @@
+package edu.neu.madcourse.numad22sp_team36_tinnews.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import edu.neu.madcourse.numad22sp_team36_tinnews.model.Article;
+
+@Database(entities = {Article.class}, version = 1, exportSchema = false)
+public abstract class TinNewsDatabase extends RoomDatabase {
+
+    public abstract ArticleDAO articleDAO();
+}
