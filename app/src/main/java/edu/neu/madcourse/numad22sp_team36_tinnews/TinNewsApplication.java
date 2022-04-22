ac668d3fc10ab7813b1c01a2134cb9a165248edb
JiangfengDong <jfdong829@gmail.com>
2022-04-21 20:30:01 -0700

    Create the database in the TinNewsApplication and add an accessor

--- app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/TinNewsApplication.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/TinNewsApplication.java
@@ -2,12 +2,26 @@ package edu.neu.madcourse.numad22sp_team36_tinnews;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
 import com.facebook.stetho.Stetho;
 
+import edu.neu.madcourse.numad22sp_team36_tinnews.database.TinNewsDatabase;
+
 public class TinNewsApplication extends Application {
+
+    private static final String DATABASE_FILE_NAME = "tinnews_db";
+
+    private static TinNewsDatabase database;
+
     @Override
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+        database = Room.databaseBuilder(this, TinNewsDatabase.class, DATABASE_FILE_NAME).build();
+    }
+
+    public static TinNewsDatabase getDatabase() {
+        return database;
     }
 }
