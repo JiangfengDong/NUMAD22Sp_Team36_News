bbc4a16acd7d4df71c5475f1bce8384830add81a
JiangfengDong <jfdong829@gmail.com>
2022-04-23 16:31:05 -0700

    Add an adapter SavedNewsAdapter that contains the ViewHolder

---
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_tinnews/ui/save/SavedNewsAdapter.java
@@ -0,0 +1,62 @@
+package edu.neu.madcourse.numad22sp_team36_tinnews.ui.save;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.neu.madcourse.numad22sp_team36_tinnews.R;
+import edu.neu.madcourse.numad22sp_team36_tinnews.databinding.SavedNewsItemBinding;
+import edu.neu.madcourse.numad22sp_team36_tinnews.model.Article;
+
+public class SavedNewsAdapter extends RecyclerView.Adapter<SavedNewsAdapter.SavedNewsViewHolder> {
+
+    private final List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public SavedNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.saved_news_item, parent, false);
+        return new SavedNewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SavedNewsViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.authorTextView.setText(article.author);
+        holder.descriptionTextView.setText(article.description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    public static class SavedNewsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView authorTextView;
+        TextView descriptionTextView;
+        ImageView favoriteIcon;
+
+        public SavedNewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            SavedNewsItemBinding binding = SavedNewsItemBinding.bind(itemView);
+            authorTextView = binding.savedItemAuthorContent;
+            descriptionTextView = binding.savedItemDescriptionContent;
+            favoriteIcon = binding.savedItemFavoriteImageView;
+        }
+    }
+}
