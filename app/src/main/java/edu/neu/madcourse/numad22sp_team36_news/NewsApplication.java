d388b247bd4293c0703860e8f474d7420b16f110
JiangfengDong <jfdong829@gmail.com>
2022-04-25 14:19:07 -0700

    Update theme style

--- app/src/main/java/edu/neu/madcourse/numad22sp_team36_news/NewsApplication.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_news/NewsApplication.java
@@ -10,7 +10,7 @@ import edu.neu.madcourse.numad22sp_team36_news.database.NewsDatabase;
 
 public class NewsApplication extends Application {
 
-    private static final String ROOM_DATABASE_FILE_NAME = "tinnews_db";
+    private static final String ROOM_DATABASE_FILE_NAME = "news_db";
 
     private static NewsDatabase database;
 
