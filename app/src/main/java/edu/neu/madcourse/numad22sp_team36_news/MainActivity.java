2938a5a879561cd4abbcf87fba283cbbc6ae8c36
Han Bao <baaaohaaan@outlook.com>
2022-04-27 21:35:15 -0700

    Add notification button

--- app/src/main/java/edu/neu/madcourse/numad22sp_team36_news/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_team36_news/MainActivity.java
@@ -1,8 +1,15 @@
 package edu.neu.madcourse.numad22sp_team36_news;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -14,11 +21,34 @@ import edu.neu.madcourse.numad22sp_team36_news.R;
 public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
+    private Button testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testBtn = findViewById(R.id.testBtn);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Test Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // notification code
+                String message = "I am Handsome!";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "Test Notification");
+                builder.setContentTitle("New Notification");
+                builder.setContentText(message);
+                builder.setSmallIcon(R.drawable.ic_favorite_24dp);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
